0a6d22c707c3ebf676949fbffa5b65cb5da9d950
omp_dev <omprakashlamichhane10@gmail.com>
2024-01-02 15:21:50 +0545

    learning bind function in js

--- app5.js
+++ app5.js
@@ -1,14 +1,15 @@
 const stockmarket={
     name:"lbbl",
     ltp:570,
-    about:function()
+    about:function(stock,ltp)
     {
-        console.log("the book close of the stock is in 2080-09-19",this.name);
+        return`the book close of the stock is in 2080-09-19 ${this.name},${stock},${ltp}`;
     }
 };
 const stockmarket1={
     name:"GBBL",
     ltp:580,
 };
-stockmarket.about.call(stockmarket1);
-console.log(stockmarket1.about);
+stockmarket.about.call(stockmarket1);//call lai chai globally indicate garxa hai arko function ko chij sajilai aafno ma garna penxa
+console.log(stockmarket.about.call(stockmarket1,"ahpc",590));
+
