15916de810933aa9512331cca855a2ea0342da3b
omp_dev <omprakashlamichhane10@gmail.com>
2024-01-03 14:14:51 +0545

    some changes in js

--- map.js
+++ map.js
@@ -6,3 +6,14 @@ console.log(square);
 
 const sqrtt=number.map(Math.sqrt);
 console.log(sqrtt);
+const persons = [
+    {firstname : "Malcom", lastname: "Reynolds"},
+    {firstname : "Kaylee", lastname: "Frye"},
+    {firstname : "Jayne", lastname: "Cobb"}
+  ];
+  const personname=persons.map((item,index,pp)=>{
+    return [item.firstname,item.lastname].join(" ");
+
+  })
+
+  console.log(personname);
