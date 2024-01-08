b48bf5b020c3738152e50daa69a05457b9e53efa
omp_dev <omprakashlamichhane10@gmail.com>
2024-01-08 14:20:07 +0545

    add dom

--- dom.js
+++ dom.js
@@ -1,7 +1,23 @@
-let h1=document.querySelector(".box1 > button");
-console.log(h1);
+//dom ma hamla k k garna sakinxa tw 
+// dom ma getelementbyid rw getelementsbyid plural huxa;
+// dom ma toggle la yadi add xaina vane add garne rw add xa vane hatune kam garxa
+// dom ma classlist bata hamla class ko list haru harna sakinxa
+// dom ma hamla class add remove garna sakkinxa
+// dom ma hamla textmanuplation haru  easily garna sakinaxa
+// dom ma htmlcollection rw nodelist hunxa 
+// querySelectorAll la hamlai nodelist dinxa index ko adhar ma hamla manuplate garnxa sakinxa
+// getattributes rw setattritues("paila","tobeadd") yasare hamla  add garna sakinxa hai
 
-function walk()
-{
-    h1.innerText="hello i am omprakash"
-}
+
+
+
+document.querySelector('.container').classList.add("container-2");
+let con=document.querySelector('.container-2').style.backgroundColor="red";
+
+let p=document.querySelector(".container > p");
+p.style.backgroundColor="green"
+p.style.fontSize="2rem";
+p.style.color="white";
+
+let li=document.querySelectorAll("li");
+li[0].classList.add("box");
