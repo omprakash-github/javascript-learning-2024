7c10bfdbd2cfa45f13a5611f0495c149621c6a27
omp_dev <omprakashlamichhane10@gmail.com>
2024-01-27 23:00:32 +0545

    some chahe in js

--- callback.js
+++ callback.js
@@ -1,17 +1,25 @@
-function sum(a,b)
-{
-    setTimeout()
-    {()=>
-        {
+// function sum(a,b)
+// {
+//     setTimeout()
+//     {()=>
+//         {
 
-            console.log(a+b);
-        },2000
+//             console.log(a+b);
+//         },2000
 
-    }
-}
-function calculator(a,b,callback)
+//     }
+// }
+// function calculator(a,b,callback)
+// {
+//       callback(a,b);
+// }
+// //call back ma chai hamla functon call garna hunna hai
+// calculator(4,5,sum);
+function oddorEven(number,callback)
 {
-      callback(a,b);
+    const result=(number%2==0)?"even":"odd";
+    callback(number,result);
 }
-//call back ma chai hamla functon call garna hunna hai
-calculator(4,5,sum);
+oddorEven(28,(number,result)=>{
+    console.log(number + 'is '+result);
+})
