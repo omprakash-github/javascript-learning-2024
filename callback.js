9ef55defe18c3762c84d256d492ddb7b6f4aeb18
omp_dev <omprakashlamichhane10@gmail.com>
2024-01-28 16:26:59 +0545

    some change in js file

--- callback.js
+++ callback.js
@@ -15,11 +15,40 @@
 // }
 // //call back ma chai hamla functon call garna hunna hai
 // calculator(4,5,sum);
-function oddorEven(number,callback)
+// function oddorEven(number,callback)
+// {
+//     const result=(number%2==0)?"even":"odd";
+//     callback(number,result);
+// }
+// oddorEven(28,(number,result)=>{
+//     console.log(number + 'is '+result);
+// })
+
+
+// function one(fn)
+// {
+//     fn();
+// console.log("hello ji i am omprakash lamichanne");
+// }
+// // function two()
+// // {
+// // console.log("hello i am function two hai");
+// // }
+// one(()=>{
+// console.log("ompakash");
+// });
+
+function add(a,b)
 {
-    const result=(number%2==0)?"even":"odd";
-    callback(number,result);
+    console.log("the sum of a +b is",a+b);
 }
-oddorEven(28,(number,result)=>{
-    console.log(number + 'is '+result);
-})
+function subract(a,b)
+{
+    console.log("the subract of a-b is",a-b);
+}
+function calculator(a,b,add,subract)
+{
+   add(a,b);
+   subract(a,b);
+}
+calculator(15,20,add,subract);
