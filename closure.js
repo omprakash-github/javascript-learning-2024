9dfe0c45d43931e98898e5087713c9c891467e90
omp_dev <omprakashlamichhane10@gmail.com>
2024-01-29 14:39:32 +0545

    adding callback.js

---
+++ closure.js
@@ -0,0 +1,21 @@
+
+//i this session we gonna lern about the closure i js which is closure vananko function lai return garunu ho hai bastab ma 
+//ou have to learn about this
+
+function add()
+{
+  const name ="mero kam cahi aad  garna ho hai guys haru ";
+
+  
+  function subract()
+  {
+      console.log("mero kam chai subract garna ho hai guys",a);
+      return ;
+    }
+    const a=14;
+    return subract;
+
+}
+
+const finaloutput=add();
+console.log(finaloutput());
