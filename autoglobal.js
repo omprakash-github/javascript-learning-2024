bcffc20000bc8cd89aad3c280b4d5d5041e73356
omp_dev <omprakashlamichhane10@gmail.com>
2024-01-30 16:58:57 +0545

    add autoglobal concept

---
+++ autoglobal.js
@@ -0,0 +1,18 @@
+//this is the concept of auto global hai
+// name ="omprakash lamichhane";
+// nameofme ="prakash lamichhane"
+// console.log(name);
+// console.log(nameofme);
+
+const name="prakash lamichhane"
+
+function detail(name)
+{
+    console.log(name);
+    hello="omprakash lamichhane";
+    console
+    console.log("name")
+}
+
+detail(name);
+console.log(name);
