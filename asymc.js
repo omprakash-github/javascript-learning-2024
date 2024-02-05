27f1fef63670e3738b48adec6e89237c4f71c1c2
omp_dev <omprakashlamichhane10@gmail.com>
2024-02-05 21:48:00 +0545

    some promise code add

--- asymc.js
+++ asymc.js
@@ -6,14 +6,56 @@
 // }).then(function(){
 //   console.log("promised is solved hai");
 // })
-const promise =new Promise(function(resolve,reject){
-  setTimeout(()=>{
-    console.log("hello this is assync task hai");
-    resolve({name:"omprakashlamichhane"});
-  },4000)
-})
-promise.then(function(user)
+// const promise =new Promise(function(resolve,reject){
+//   setTimeout(()=>{
+//     console.log("hello this is assync task hai");
+//     resolve({name:"omprakashlamichhane"});
+//   },4000)
+// })
+// promise.then(function(user)
+// {
+//   console.log(user);
+//   console.log("this is the resolve statement hai dost")
+// })
+const promiseFour=new Promise(function(resolve,reject)
 {
-  console.log(user);
-  console.log("this is the resolve statement hai dost")
+  setTimeout(function(){
+    let error=true;
+    if(error)
+    {
+      resolve({username:"omprakash lamichhane",password:"123"});
+    }
+    else{
+      reject('error something went wrong');
+    }
+  },1000)
 })
+promiseFour.then((user)=>{
+  console.log(user);
+  return user.username
+
+}).then((username)=>{
+  console.log(username);
+}).catch(function(error){
+  console.log(error);
+}).finally(()=>console.log("the promise is finally resolve or rejected hai"));
+
+const promiseFive=new Promise(function(resolve,reject){
+  setTimeout(function(){
+    let error=true;
+    if(!error){
+      resolve({username:"javascript",password:"123"})
+
+    }
+    else{
+      reject('ERROR:JS went wrong');
+    }
+    
+  }, 1000)
+});
+
+async function consumepromiseFive(){
+  const response=await promiseFive;
+  console.log(response);
+}
+consumepromiseFive();
