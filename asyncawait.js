0934283303abeebad94f521b8468dcf0c1d5ac18
omp_dev <omprakashlamichhane10@gmail.com>
2024-02-10 23:33:04 +0545

    async

--- asyncawait.js
+++ asyncawait.js
@@ -7,15 +7,28 @@
 // student();
 // console.log("my name is sarada lamichhane");
 // console.log("my name is paltan lamichhane");
+// async function count()
+// {
+//     console.log("1");
+//     console.log("2");
+//     console.log("3");
+//     await console.log("4");
+//     console.log("5");
+//     console.log("6");
+// // }
+// console.log("async rw await lai chai majala bugna parxa la guys aru ma bugauxu");
+// count();
+// console.log("i ma 8 hai tw guys ");
 async function count()
 {
     console.log("1");
+    await console.log("yo chai network request ho hai");
+    console.log("1");
     console.log("2");
-    console.log("3");
-    await console.log("4");
     console.log("5");
     console.log("6");
+  
 }
-console.log("async rw await lai chai majala bugna parxa la guys aru ma bugauxu");
+console.log("my name is omprakash lamichahne");
 count();
-console.log("i ma 8 hai tw guys ");
+console.log("my name is prakash lamichahne");
